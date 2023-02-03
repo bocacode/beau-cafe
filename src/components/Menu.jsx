f835dbcb896fea8ea970026bf679476b9fb82f79
Todd Albert <todd@bocacode.com>
2023-02-03 11:02:33 -0500

    bringing in coffees

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+
 export default function Menu() {
+  const [coffees, setCoffees] = useState()
+  const getCoffees = (temp) => {
+    fetch(`https://api.sampleapis.com/coffee/${temp}`)
+      .then(res => res.json())
+      .then(setCoffees)
+      .catch(alert)
+  }
   return (
     <article>
-      <h2>Welcome to the Beau Café</h2>
+      <div>
+        <button onClick={() => getCoffees('hot')}>Hot Coffees</button>
+        <button onClick={() => getCoffees('iced')}>Iced Coffees</button>
+      </div>
+      {!coffees
+        ? <h2>Welcome to the Beau Café</h2>
+        : <ul>
+          {coffees.map(coffee => (
+            <li key={coffee.id}>{coffee.title}</li>
+          ))}
+        </ul>
+      }
     </article>
   );
 }
